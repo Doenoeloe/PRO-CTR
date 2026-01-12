69011d6e37ae720a86ac562e4b2a7d55cdc27d96
Deniz1231 <deniz200712@hotmail.com>
2026-01-12 09:20:05 +0100

    playerMovement script fix

--- PRO-CTR/Assets/Scripts/PlayerMovement.cs
+++ PRO-CTR/Assets/Scripts/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool isMoving = false;
 
-    bool up, down, right , left;
+    bool up, down, right, left;
 
     private void Awake()
     {
@@ -26,54 +26,61 @@ public class PlayerMovement : MonoBehaviour
 
         if (grid == null)
         {
-            grid = FindObjectOfType<Grid>();    
-        }
-
-        void Update()
-        {
-            if (isRepeatedMovement)
-            {
-                up = Input.GetKey(KeyCode.UpArrow);
-                down = Input.GetKey(KeyCode.DownArrow);
-                left = Input.GetKey(KeyCode.LeftArrow);
-                right = Input.GetKey(KeyCode.RightArrow);
-            }
-            else
-            {
-                up = Input.GetKey(KeyCode.UpArrow);
-                down = Input.GetKey(KeyCode.DownArrow);
-                left = Input.GetKey(KeyCode.LeftArrow);
-                right = Input.GetKey(KeyCode.RightArrow);
-
-            }
-
-            Vector2Int direction = Vector2Int.zero;
-
-            if (up)
-            {
-                direction = Vector2Int.up;
-            }
-            else if (down)
-            {
-                direction = Vector2Int.down;
-            }
-            else if (left)
-            {
-                direction = Vector2Int.left;
-            }
-            else if (right)
-            {
-                direction = Vector2Int.right;
-            }
-
-            if (direction != Vector2Int.zero)
-            {
-                TryMove(direction);
-            }
-
-
+            grid = FindObjectOfType<Grid>();
         }
     }
+
+    private void Start()
+    {
+        Vector3Int cell = grid.WorldToCell(transform.position);
+        transform.position = grid.GetCellCenterWorld(cell);
+    }
+
+    void Update()
+    {
+        if (isRepeatedMovement)
+        {
+            up = Input.GetKey(KeyCode.UpArrow);
+            down = Input.GetKey(KeyCode.DownArrow);
+            left = Input.GetKey(KeyCode.LeftArrow);
+            right = Input.GetKey(KeyCode.RightArrow);
+        }
+        else
+        {
+            up = Input.GetKey(KeyCode.UpArrow);
+            down = Input.GetKey(KeyCode.DownArrow);
+            left = Input.GetKey(KeyCode.LeftArrow);
+            right = Input.GetKey(KeyCode.RightArrow);
+
+        }
+
+        Vector2Int direction = Vector2Int.zero;
+
+        if (up)
+        {
+            direction = Vector2Int.up;
+        }
+        else if (down)
+        {
+            direction = Vector2Int.down;
+        }
+        else if (left)
+        {
+            direction = Vector2Int.left;
+        }
+        else if (right)
+        {
+            direction = Vector2Int.right;
+        }
+
+        if (direction != Vector2Int.zero)
+        {
+            TryMove(direction);
+        }
+
+
+    }
+
 
     private void TryMove(Vector2Int direction)
     {
@@ -92,13 +99,13 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 targetWorldPosition = grid.GetCellCenterWorld(targetCell);
         StartCoroutine(Move(targetWorldPosition));
-        
+
 
     }
     private IEnumerator Move(Vector3 targetPosition)
     {
         isMoving = true;
-
+         
         Vector3 startPosition = transform.position;
         float elapsedTime = 0f;
 

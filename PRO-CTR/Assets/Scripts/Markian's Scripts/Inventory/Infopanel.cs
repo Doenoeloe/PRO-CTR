3ba26a310f976508e865cca4948a0d6639dd53b8
Markian Tsypuk <markianzypuk@gmail.com>
2026-01-15 13:29:04 +0100

    Script commenting
    All the inventory scripts are under comment to keep the game from errors due to missing Item script.

--- PRO-CTR/Assets/Scripts/Markian's Scripts/Inventory/Infopanel.cs
+++ PRO-CTR/Assets/Scripts/Markian's Scripts/Inventory/Infopanel.cs
@@ -1,34 +1,34 @@
-using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class Infopanel : MonoBehaviour
-{
-    [Header("UI")]
-    [SerializeField] public Image InfoPanelImage;
-    [SerializeField] public TMP_Text InfoPanelName;
-    [SerializeField] public TMP_Text InfoPanelDescription;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+//public class Infopanel : MonoBehaviour
+//{
+//    [Header("UI")]
+//    [SerializeField] public Image InfoPanelImage;
+//    [SerializeField] public TMP_Text InfoPanelName;
+//    [SerializeField] public TMP_Text InfoPanelDescription;
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
-    public void DisplayDetails(Item pItem)
-    {
-        InfoPanelName.text = pItem.name;
-        InfoPanelImage = pItem.image;
-        InfoPanelDescription.text = pItem.description;
-    }
-    public void HideDetails()
-    {
-        InfoPanelName.text = null;
-        InfoPanelImage = null;
-        InfoPanelDescription.text = null;
-    }
-}
+//    }
+//    public void DisplayDetails(Item pItem)
+//    {
+//        InfoPanelName.text = pItem.name;
+//        InfoPanelImage = pItem.image;
+//        InfoPanelDescription.text = pItem.description;
+//    }
+//    public void HideDetails()
+//    {
+//        InfoPanelName.text = null;
+//        InfoPanelImage = null;
+//        InfoPanelDescription.text = null;
+//    }
+//}
--- PRO-CTR/Assets/Scripts/Markian's Scripts/Inventory/InventoryUI.cs
+++ PRO-CTR/Assets/Scripts/Markian's Scripts/Inventory/InventoryUI.cs
@@ -1,34 +1,34 @@
-using UnityEngine;
-using System.Collections.Generic;
-public class InventoryUI : MonoBehaviour
-{
-    Inventory inventory;
-    [SerializeField] Infopanel infoPanel;
-    //place where all the cells are located
-    [SerializeField] GameObject cellGrid;
-    [SerializeField] GameObject cellPrefab;
-    List<GameObject> cells;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        inventory = GetComponent<Inventory>();
-    }
-    void CreateCellUI(Item pItem)
-    {
-        var cell = Instantiate(cellPrefab, cellGrid.transform, false);
-        cell.GetComponent<InventoryCell>().AssignCell(pItem, infoPanel);
-        cells.Add(cell);
-    }
-    void Refresh()
-    {
-        foreach (var cell in cells)
-        {
-            Destroy(cell);
-        }
-        cells.Clear();
-        foreach (Item item in inventory.GetItems())
-        {
-            CreateCellUI(item);
-        }
-    }
-}
+//using UnityEngine;
+//using System.Collections.Generic;
+//public class InventoryUI : MonoBehaviour
+//{
+//    Inventory inventory;
+//    [SerializeField] Infopanel infoPanel;
+//    //place where all the cells are located
+//    [SerializeField] GameObject cellGrid;
+//    [SerializeField] GameObject cellPrefab;
+//    List<GameObject> cells;
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+//        inventory = GetComponent<Inventory>();
+//    }
+//    void CreateCellUI(Item pItem)
+//    {
+//        var cell = Instantiate(cellPrefab, cellGrid.transform, false);
+//        cell.GetComponent<InventoryCell>().AssignCell(pItem, infoPanel);
+//        cells.Add(cell);
+//    }
+//    void Refresh()
+//    {
+//        foreach (var cell in cells)
+//        {
+//            Destroy(cell);
+//        }
+//        cells.Clear();
+//        foreach (Item item in inventory.GetItems())
+//        {
+//            CreateCellUI(item);
+//        }
+//    }
+//}
